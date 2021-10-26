06568f0b79ba21b0dda9de0a2efe069ac4210216
PhlegmaticOne <73738250+PhlegmaticOne@users.noreply.github.com>
2021-10-26 18:42:54 +0300

    Added main models from task

--- TransportEnterprise.Domain/TruckTractors/ActrosMP2.cs
+++ TransportEnterprise.Domain/TruckTractors/ActrosMP2.cs
@@ -2,9 +2,7 @@
 {
     public class ActrosMP2 : TruckTractor
     {
-        public ActrosMP2(Semitrailer<Product> semitrailer) : base(semitrailer)
-        {
-        }
+        public ActrosMP2(Semitrailer<Product> semitrailer) : base(semitrailer) { }
 
         public override decimal PetrolPerHour => 1.1m * Semitrailer.LoadCapacity;
     }
